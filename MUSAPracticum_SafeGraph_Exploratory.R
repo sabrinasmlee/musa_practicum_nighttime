90f53beedfe6bf27ec88f41d1bec9fd680b1aaac
mlkornhauser <70709544+mlkornhauser@users.noreply.github.com>
2021-02-04 22:22:11 -0500

    Update MUSAPracticum_SafeGraph_Exploratory.R

--- MUSAPracticum_SafeGraph_Exploratory.R
+++ MUSAPracticum_SafeGraph_Exploratory.R
@@ -11,6 +11,34 @@ library(lubridate)
 library(datetime)
 library(viridis)
 
+qBr <- function(df, variable, rnd) {
+  if (missing(rnd)) {
+    as.character(quantile(round(df[[variable]],0),
+                          c(.01,.2,.4,.6,.8), na.rm=T))
+  } else if (rnd == FALSE | rnd == F) {
+    as.character(formatC(quantile(df[[variable]]), digits = 3),
+                 c(.01,.2,.4,.6,.8), na.rm=T)
+  }
+}
+q5 <- function(variable) {as.factor(ntile(variable, 5))}
+palette5 <- viridis_pal()(5)
+
+mapTheme <- function(base_size = 12) {
+  theme(
+    text = element_text( color = "black"),
+    plot.title = element_text(size = 14,colour = "black"),
+    plot.subtitle=element_text(face="italic"),
+    plot.caption=element_text(hjust=0),
+    axis.ticks = element_blank(),
+    panel.background = element_blank(),axis.title = element_blank(),
+    axis.text = element_blank(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_rect(colour = "black", fill=NA, size=2)
+  )
+}
+
 setwd("~/GitHub/musa_practicum_nighttime")
 
 ###########
@@ -32,31 +60,6 @@ dat <- dat %>%
 
 colnames(dat)
 head(dat)
-
-#Business traffic by day
-dat_day <- 
-  dat %>% 
-  select(safegraph_place_id, popularity_by_day) %>%
-  mutate(popularity_by_day = str_remove_all(popularity_by_day, pattern = "\\[|\\]")) %>%
-  mutate(popularity_by_day = str_remove_all(popularity_by_day, pattern = "\\{|\\}")) %>%
-  mutate(popularity_by_day = str_remove_all(popularity_by_day, pattern = '\\"|\\"')) %>% 
-  #mutate(popularity_by_day = str_replace_all(popularity_by_day, "[:]", " ")) %>% 
-  mutate(popularity_by_day = str_split(popularity_by_day, pattern = ",")) %>%
-  unnest(popularity_by_day) %>%
-  separate(.,
-           popularity_by_day,
-           c("Day", "Visits"),
-           sep = ":") %>%
-  mutate(Visits = as.numeric(Visits))
-
-week_order = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-
-dat_day %>%
-  group_by(Day) %>%
-  summarize(Avg_Visits = mean(Visits)) %>%
-  mutate(Day = factor(Day, levels = week_order)) %>% arrange(Day) %>% #order by day of the week
-  ggplot(., aes(x = Day, y = Avg_Visits)) + 
-  geom_col()
 
 #Popularity by Hour
 dat_hour <-
@@ -132,7 +135,7 @@ dat_cbg <-
             Avg_Visitors = mean(raw_visitor_counts),
             Avg_Dwell = mean(median_dwell),
             Avg_DistHome = mean(distance_from_home)) %>%
-  full_join(phl_cbg) %>% #lots of cbgs without SG POIs, so full join instead of left join.
+  left_join(phl_cbg) %>% 
   st_as_sf()
 
 #Average number of visits by CBG
@@ -162,7 +165,7 @@ dat_cbg %>%
   scale_fill_viridis() +
   labs(title = "Average SafeGraph Dwell \nTime per POI by CBG")
 
-#Average dist from home by zip code
+#Average dist from home by CBG
 dat_cbg %>%
   subset(Avg_DistHome < 9000) %>% #removing outliers to see patterns in the data
   ggplot() + 
@@ -194,3 +197,123 @@ dat_cbg %>%
 
 # ^^Figure out how to join this to the phl_cbg map.  
 #See which cbgs hve the highest number of people coming from ouside.
+
+################################################
+# Question 1: Where are people travelling from?
+################################################
+
+dat_cbg_visitors <- 
+  dat %>%
+  select(safegraph_place_id, poi_cbg, visitor_home_cbgs) %>%
+  mutate(visitor_home_cbgs = str_remove_all(visitor_home_cbgs, pattern = "\\[|\\]")) %>%
+  mutate(visitor_home_cbgs = str_remove_all(visitor_home_cbgs, pattern = "\\{|\\}")) %>%
+  mutate(visitor_home_cbgs = str_remove_all(visitor_home_cbgs, pattern = '\\"|\\"')) %>%
+  mutate(visitor_home_cbgs = str_split(visitor_home_cbgs, pattern = ",")) %>%
+  unnest(visitor_home_cbgs) %>%
+  separate(.,
+           visitor_home_cbgs,
+           c("Visitor_CBG", "Visitors"),
+           sep = ":") %>%
+  mutate(Visitor_CBG = as.numeric(Visitor_CBG),
+         poi_cbg = as.numeric(poi_cbg),
+         Visitors = as.numeric(Visitors))
+
+dat_cbg_visitors %>%
+  group_by(poi_cbg) %>%
+  summarize(Count = n()) %>%
+  rename(., GEOID10 = poi_cbg) %>%
+  left_join(phl_cbg) %>%
+  st_as_sf() %>%
+  ggplot() + 
+  geom_sf(aes(fill = q5(Count)), color = "transparent") + 
+  scale_fill_manual(values = palette5,
+                    name = "CBG Count\nQuintile Breaks") + 
+  mapTheme() +
+  labs(title = "How many different CBGs visit the POI CBG?")
+
+dat_zip_visitors <- 
+  dat %>%
+  select(safegraph_place_id, postal_code, visitor_home_cbgs) %>%
+  mutate(visitor_home_cbgs = str_remove_all(visitor_home_cbgs, pattern = "\\[|\\]")) %>%
+  mutate(visitor_home_cbgs = str_remove_all(visitor_home_cbgs, pattern = "\\{|\\}")) %>%
+  mutate(visitor_home_cbgs = str_remove_all(visitor_home_cbgs, pattern = '\\"|\\"')) %>%
+  mutate(visitor_home_cbgs = str_split(visitor_home_cbgs, pattern = ",")) %>%
+  unnest(visitor_home_cbgs) %>%
+  separate(.,
+           visitor_home_cbgs,
+           c("Visitor_CBG", "Visitors"),
+           sep = ":") %>%
+  mutate(Visitor_CBG = as.numeric(Visitor_CBG),
+         postal_code = as.numeric(postal_code),
+         Visitors = as.numeric(Visitors))
+
+dat_zip_visitors %>%
+  group_by(postal_code) %>%
+  summarize(Count = n()) %>%
+  rename(., CODE = postal_code) %>%
+  left_join(phl_zip) %>%
+  st_as_sf() %>%
+  ggplot() + 
+  geom_sf(aes(fill = q5(Count)), color = "transparent") + 
+  scale_fill_manual(values = palette5,
+                    name = "CBG Count\nQuintile Breaks") + 
+  mapTheme() +
+  labs(title = "How many different CBGs visit the POI CBG?")
+
+########################################
+# Question 2: When are People Travelling
+########################################
+#Business traffic by day
+dat_day <- 
+  dat %>% 
+  select(safegraph_place_id, popularity_by_day) %>%
+  mutate(popularity_by_day = str_remove_all(popularity_by_day, pattern = "\\[|\\]")) %>%
+  mutate(popularity_by_day = str_remove_all(popularity_by_day, pattern = "\\{|\\}")) %>%
+  mutate(popularity_by_day = str_remove_all(popularity_by_day, pattern = '\\"|\\"')) %>% 
+  mutate(popularity_by_day = str_split(popularity_by_day, pattern = ",")) %>%
+  unnest(popularity_by_day) %>%
+  separate(.,
+           popularity_by_day,
+           c("Day", "Visits"),
+           sep = ":") %>%
+  mutate(Visits = as.numeric(Visits))
+
+week_order = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+dat_day %>%
+  group_by(Day) %>%
+  summarize(Avg_Visits = mean(Visits)) %>%
+  mutate(Day = factor(Day, levels = week_order)) %>% arrange(Day) %>% #order by day of the week
+  ggplot(., aes(x = Day, y = Avg_Visits)) + 
+  geom_col()
+
+# dat_day %>%
+#   group_by(Day) %>%
+#   summarize(Count = n()) %>%
+#   mutate(Day = factor(Day, levels = week_order)) %>% arrange(Day) %>% #order by day of the week
+#   ggplot(., aes(x = Day, y = Count)) + 
+#   geom_col()
+
+dat_hour <- 
+  dat %>% 
+  select(safegraph_place_id, popularity_by_hour) %>%
+  mutate(popularity_by_hour = str_remove_all(popularity_by_hour, pattern = "\\[|\\]")) %>%
+  unnest(popularity_by_hour) %>%
+  separate(.,
+           popularity_by_hour,
+           c("0", "1", "2", "3", "4", "5", "6", 
+             "7", "8", "9", "10", "11", "12", 
+             "13", "14", "15", "16", "17", "18",
+             "19", "20", "21", "22", "23"),
+           sep = ",") %>%
+  pivot_longer(cols = 2:25,
+               names_to = "Hour",
+               values_to = "Count") %>%
+  mutate(Hour = as.numeric(Hour),
+         Count = as.numeric(Count))
+  
+dat_hour %>%
+  group_by(Hour) %>%
+  summarize(Count = sum(Count)) %>%
+  ggplot(., aes(x = Hour, y = Count)) + 
+  geom_col()
